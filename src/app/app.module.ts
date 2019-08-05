b405c0c1a6c4c0b122661088f8f3fbe1999ea4c2
Alisher <alisher.aliev@tieto.com>
2019-08-05 10:31:30 +0300

    Created EventList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { EventsListComponent } from './events/events-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html'
+})
+export class EventsListComponent {
+  event = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '9/26/2019',
+    time: '10:00 am',
+    price: 599.99,
+    imageUrl: 'assets/images/angularconnect-shield.png',
+    location: {
+      city: 'Tashkent',
+      country: 'Uzbekistan'
+    }
+  };
+}
