2280260d02cf36a4693fd3ffaa9ba7c267e081cc
Alisher <alisher.aliev@tieto.com>
2019-12-09 15:43:35 +0200

    use styles: []

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,7 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event-thumbnail',
-  templateUrl: './event-thumbnail.component.html'
+  templateUrl: './event-thumbnail.component.html',
+  styles: [`
+    .pad-left { margin-left: 10px;}
+    .well div { color: #bbb; }
+  `]
 })
 export class EventThumbnailComponent {
   @Input() event: any;
