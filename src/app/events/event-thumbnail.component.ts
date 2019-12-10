3a231aedc62108514b587f57ecb090187baa3e88
Alisher <alisher.aliev@tieto.com>
2019-12-10 10:12:17 +0200

    use Style Binding

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,8 +4,6 @@ import { Component, Input } from '@angular/core';
   selector: 'app-event-thumbnail',
   templateUrl: './event-thumbnail.component.html',
   styles: [`
-  .green { color: green !important; }
-  .bold { font-weight: bold; }
   .thumbnail { min-height: 210px; }
   .pad-left { margin-left: 10px; }
   .well div { color: #bbb; }
@@ -18,13 +16,5 @@ export class EventThumbnailComponent {
 
   logFoo() {
     console.log('foo');
-  }
-
-  getStatTimeClass() {
-    if (this.event && this.event.time === '8:00 am') {
-      return ['green', 'bold'];
-    } else {
-      return '';
-    }
   }
 }
