c06bf4121ddb2670d76ca2b387a4d73b15f38106
Alisher <alisher.aliev@tieto.com>
2019-12-10 13:26:52 +0200

    create ToastrService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ToastrService } from './common/toastr.service';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventService } from './events/shared/event.service';
@@ -17,7 +18,10 @@ import { NavBarComponent } from './nav/navbar.component';
   imports: [
     BrowserModule
   ],
-  providers: [EventService],
+  providers: [
+    EventService,
+    ToastrService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from '../common/toastr.service';
 import { EventService } from './shared/event.service';
 
-declare let toastr;
 @Component({
   selector: 'app-events-list',
   templateUrl: './events-list.component.html'
@@ -9,13 +9,13 @@ declare let toastr;
 export class EventsListComponent implements OnInit {
   events: any[];
 
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService, private toastr: ToastrService) { }
 
   ngOnInit() {
     this.events = this.eventService.getEvents();
   }
 
   handleThumbnailClick(eventName: string) {
-    toastr.success(eventName);
+    this.toastr.success(eventName);
   }
 }
