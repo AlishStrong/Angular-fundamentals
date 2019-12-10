c45fbe13fb6960b43d585cd18b0a55c4a34c31b6
Alisher <alisher.aliev@tieto.com>
2019-12-10 13:08:19 +0200

    install toastr
    npm install toastr --save
    toastr.min.js must go after jquery

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 
+declare let toastr;
 @Component({
   selector: 'app-events-list',
   templateUrl: './events-list.component.html'
@@ -12,5 +13,9 @@ export class EventsListComponent implements OnInit {
 
   ngOnInit() {
     this.events = this.eventService.getEvents();
+  }
+
+  handleThumbnailClick(eventName: string) {
+    toastr.success(eventName);
   }
 }
