b1abab6bb981ccc93de819e216e9ce8455718dea
Alisher <alisher.aliev@tieto.com>
2019-12-12 12:52:45 +0200

    implement Route Guard
    Create EventRouteActivator to redirect to 404 page if the requested event does not exist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ToastrService } from './common/toastr.service';
 import { Error404Component } from './errors/error404.component';
 import { CreateEventComponent } from './events/create-event.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventService } from './events/shared/event.service';
@@ -29,7 +30,8 @@ import { appRoutes } from './routes';
   ],
   providers: [
     EventService,
-    ToastrService
+    ToastrService,
+    EventRouteActivator
   ],
   bootstrap: [AppComponent]
 })
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { Error404Component } from './errors/error404.component';
 import { CreateEventComponent } from './events/create-event.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventsListComponent } from './events/events-list.component';
 
 export const appRoutes: Routes = [
   { path: '404', component: Error404Component },
   { path: 'events', component: EventsListComponent },
   { path: 'events/new', component: CreateEventComponent },
-  { path: 'events/:id', component: EventDetailsComponent },
+  { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
   { path: '', redirectTo: '/events', pathMatch: 'full' }
 ];
