50a1cc5e3063d9a924b77a28f045f6f927e4dae0
Alisher <alisher.aliev@tieto.com>
2020-01-28 15:53:22 +0200

    Dive deeper into template-based forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { AuthService } from './user/auth.service';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [
     EventService,
