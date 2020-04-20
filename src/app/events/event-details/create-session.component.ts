561f0d93710f4d4d19f62a53609ce3898ddb6dfe
Alisher <alisher.aliev@tieto.com>
2020-04-20 17:18:46 +0300

    Custom validator creation

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ISession } from '../shared/event.model';
+import { ISession, restrictedWords } from '../shared';
 
 @Component({
   templateUrl: './create-session.component.html',
   styles: [`
   em { float: right; color: #E05C65; padding-left: 10px; }
   .error input { background-color: #E3C3C5 }
+  .error textarea { background-color: #E3C3C5 }
   .error ::-webkit-input-placeholder { color: #999; }
   .error ::-moz-placeholder { color: #999; }
   .error :-moz-placeholder { color: #999; }
@@ -26,7 +27,7 @@ export class CreateSessionComponent implements OnInit {
     this.presenter = new FormControl('', Validators.required);
     this.duration = new FormControl('', Validators.required);
     this.level = new FormControl('', Validators.required);
-    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar'])]);
 
     this.newSessionForm = new FormGroup({
       name: this.name,
