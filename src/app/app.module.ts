3d9cd683d336aceeb142e28bace04b96bf0ef09c
AlishStrong <alishet1990@gmail.com>
2020-05-09 02:45:24 +0300

    Wrapped jQuery into Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { CollapsibleWellComponent } from './common/collapsible-well.component';
-import { Toastr, TOASTR_TOKEN } from './common/toastr.service';
+import { Toastr, TOASTR_TOKEN, JQ_TOKEN, CollapsibleWellComponent } from './common/index';
 import { Error404Component } from './errors/error404.component';
 import { CreateEventComponent, CreateSessionComponent, DurationPipe, EventDetailsComponent, EventListResolver, EventRouteActivator, EventService, EventsListComponent, EventThumbnailComponent, SessionListComponent } from './events/index';
 import { NavBarComponent } from './nav/navbar.component';
@@ -12,6 +11,8 @@ import { appRoutes } from './routes';
 import { AuthService } from './user/auth.service';
 
 const toastr: Toastr = window['toastr'];
+const jQuery = window['$'];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -37,6 +38,10 @@ const toastr: Toastr = window['toastr'];
     {
       provide: TOASTR_TOKEN,
       useValue: toastr
+    },
+    {
+      provide: JQ_TOKEN,
+      useValue: jQuery
     },
     EventRouteActivator,
     {
