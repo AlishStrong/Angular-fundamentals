d241cdada9cb69a98b64233e30900724f4787257
Alisher <alisher.aliev@tieto.com>
2020-05-10 20:29:17 +0300

    Implement Authentication

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,17 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { IUser } from './user.model';
 
 @Injectable()
 export class AuthService {
+  constructor(private http: HttpClient) { }
+
   currentUser: IUser;
 
-  loginUser(userName: string, password: string): void {
-    this.currentUser = {
-      id: 1,
-      userName: 'alisheraliev',
-      firstName: 'Alisher',
-      lastName: 'Aliev'
+  loginUser(userName: string, password: string): Observable<any> {
+    const url = '/api/login';
+    const loginInfo = {
+      username: userName,
+      password
     };
+    const options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post(url, loginInfo, options)
+      .pipe(tap(data => {
+        this.currentUser = data['user'];
+      }))
+      .pipe(catchError(err => {
+        return of(false);
+      }));
   }
 
   isAuthenticated(): boolean {
